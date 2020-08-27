46f11349611accf158acc28f4f89cb438caa2a73
Santiago <santiagorodriguez17@seven4n.com>
2020-08-27 11:37:13 -0500

    feat(appjs): updare klasjflk adsjf
    kasdjfklasjdflkdasjflkasdjj dsfsadkljflk asdjfk ljsdaklfj adslkfj alksdlksadjf lkasd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. BUG FIXING
+          Edit <code>src/App.js</code> and save to reload. BUG FIXING review
         </p>
         <a
           className="App-link"

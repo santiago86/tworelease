1fc4956b950bba4d0f19da3b941e98459cfb6e10
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-27 16:39:36 +0000

    Deploy santiago86/tworelease to github.com/santiago86/tworelease.git:gh-pages

---
+++ precache-manifest.dcc257c4fde8ed8110816ef6c0eb5574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c6812cb51d68d9e4ad3251c993fb60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2314073c153ffcde6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "120994d59d53c4ec339a",
+    "url": "/static/js/2.537627d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.537627d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2314073c153ffcde6b",
+    "url": "/static/js/main.29fe3588.chunk.js"
+  },
+  {
+    "revision": "52de2cad55e420cf6494",
+    "url": "/static/js/runtime-main.425346a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
